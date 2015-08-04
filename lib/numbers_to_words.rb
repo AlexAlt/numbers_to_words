a99f4f12749dc931ba8b241293d8422ebab85aaf
Alex M Altieri & Rachael A Berecka <student@epicodus.com>
2015-08-04 16:16:15 -0700

    translate a two digit numeric number into written form

--- lib/numbers_to_words.rb
+++ lib/numbers_to_words.rb
@@ -1,6 +1,29 @@
+require('pry')
+
 class Fixnum
   define_method(:numbers_to_words) do
-    single_digits = {1 => "one", 2 => "two", 3 => "three", 4 => "four", 5 => "five", 6 => "six", 7 => "seven", 8 => "eight", 9 => "nine" }
-        single_digits.fetch(self)
+    ones_digits = {"1" => "one", "2" => "two", "3" => "three", "4" => "four", "5" => "five", "6" => "six", "7" => "seven", "8"=> "eight", "9" => "nine"}
+    tens_digits = {"2" => "twenty", "3" => "thirty", "4" => "forty", "5" => "fifty", "6" => "sixty", "7" => "seventy", "8"=> "eighty", "9"=>"ninety"}
+    number_string = self.to_s() 
+    digit_array = number_string.split("")
+
+    final_number = []
+
+    if digit_array.length() == 2
+      digit_array.each_with_index() do |digit, index|
+
+        if index == 0
+          tens_written = tens_digits.fetch(digit)
+          final_number.push(tens_written)
+        else
+          ones_written = ones_digits.fetch(digit)
+          final_number.push(ones_written)
+        end
+
+      end
+      final_number.join("")
+    else
+      ones_digits.fetch(number_string)
+    end
   end
 end
--- spec/numbers_to_words_spec.rb
+++ spec/numbers_to_words_spec.rb
@@ -5,4 +5,7 @@ describe('Fixnum#numbers_to_words') do
   it("Translates number in numerical form to written form") do
     expect(5.numbers_to_words()).to(eq("five"))
   end
+  it("Translates a two digit number blah") do
+    expect(25.numbers_to_words()).to(eq("twentyfive"))
+  end
 end
